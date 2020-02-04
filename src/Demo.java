9796224e23f68cb9221e0a8dd33bf0a78ef805d6
Denis Danov <Denis.Danov@vistajet.com>
2020-02-04 21:09:45 +0200

    my first commit

---
+++ src/Demo.java
@@ -0,0 +1,71 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		// System.out.println(Integer.parseInt("adsa"));
+
+		// String line = "hellojava";
+		// System.out.println(line.substring(0, 5));
+
+		// System.out.println(" asdads adasdasd ".replace(" ", ""));
+
+		// String line = "Hello java java java";
+		// System.out.println(line.replace("java", "world"));
+
+		// String line = "Hello my name is Peter";
+		// System.out.println(line.indexOf("Peter"));
+
+		// String firstWord = "aaabb";
+		// String secondWord = "Aaabb";
+		//// System.out.println(firstWord.equals(secondWord));
+		// System.out.println(firstWord.compareTo(secondWord));
+
+		// String a = "Hello ";
+		// String b = "Java";
+		// a = a+b;
+		//// System.out.println(a+b);
+		//
+		// System.out.println(a);
+
+		// int[] arr = { 3, 9, 1, 2, 5 };
+		// Arrays.sort(arr);
+		// int i = Arrays.binarySearch(arr, 9);
+		//
+		// System.out.println(i);
+
+		// int[] arr = { 3, 9, 1, 2, 5 };
+		// for (int el : arr) {
+		// System.out.printf("%d ",el);
+		// }
+		// System.out.println();
+		// Arrays.sort(arr);
+		// for (int el : arr) {
+		// System.out.printf("%d ",el);
+		// }
+
+		// int[][] arr1 = new int[2][3];
+
+		// int[][] arr = new int[][] {
+		// {1,2,3},
+		// {4,5}
+		// };
+		//
+		// int[][] arr = new int[2][];
+		// arr[0] = new int[]{1,2,3};
+		//
+		// System.out.println(arr[0][2]);
+
+		// for(int i = 0 ; i < arr.length; i++) {
+		// for(int j = 0; j < arr[i].length; j++) {
+		// System.out.println(arr[i][j]);
+		// }
+		// }
+
+		// int[] arr = new int[5];
+		//
+		//
+		// for(int i = 0; i < arr.length; i++) {
+		// System.out.println(arr[i]);
+		// }
+	}
+}
